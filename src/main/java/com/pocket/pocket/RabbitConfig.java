e5e7db7df0c350853fa93f4ed18f0bfbbe4dae64
Syed Nazmus Sakib <nazmussak111@gmail.com>
2026-08-11 16:57:58 +0600

    Add retry with exponential backoff and dead-letter queue for failed syncs

--- src/main/java/com/pocket/pocket/RabbitConfig.java
+++ src/main/java/com/pocket/pocket/RabbitConfig.java
@@ -1,6 +1,10 @@
 package com.pocket.pocket;
 
+import org.springframework.amqp.core.Binding;
+import org.springframework.amqp.core.BindingBuilder;
+import org.springframework.amqp.core.DirectExchange;
 import org.springframework.amqp.core.Queue;
+import org.springframework.amqp.core.QueueBuilder;
 import org.springframework.amqp.support.converter.JacksonJsonMessageConverter;
 import org.springframework.amqp.support.converter.MessageConverter;
 import org.springframework.context.annotation.Bean;
@@ -10,10 +14,31 @@ import org.springframework.context.annotation.Configuration;
 public class RabbitConfig {
 
     public static final String SYNC_QUEUE = "account.sync.queue";
+    public static final String DEAD_LETTER_EXCHANGE = "account.sync.dlx";
+    public static final String DEAD_LETTER_QUEUE = "account.sync.queue.dlq";
+    public static final String DEAD_LETTER_ROUTING_KEY = "account.sync.dead";
 
     @Bean
     public Queue syncQueue() {
-        return new Queue(SYNC_QUEUE, true);
+        return QueueBuilder.durable(SYNC_QUEUE)
+            .withArgument("x-dead-letter-exchange", DEAD_LETTER_EXCHANGE)
+            .withArgument("x-dead-letter-routing-key", DEAD_LETTER_ROUTING_KEY)
+            .build();
+    }
+
+    @Bean
+    public DirectExchange deadLetterExchange() {
+        return new DirectExchange(DEAD_LETTER_EXCHANGE);
+    }
+
+    @Bean
+    public Queue deadLetterQueue() {
+        return QueueBuilder.durable(DEAD_LETTER_QUEUE).build();
+    }
+
+    @Bean
+    public Binding deadLetterBinding() {
+        return BindingBuilder.bind(deadLetterQueue()).to(deadLetterExchange()).with(DEAD_LETTER_ROUTING_KEY);
     }
 
     @Bean
